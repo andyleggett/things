ead114a145c5f079698dd9892a802cf5441bf41a
Andrew Leggett <me@andyleggett.co.uk>
2017-07-19 20:43:40 +0100

    Subject

---
+++ monads/subject.js
@@ -0,0 +1,37 @@
+const { append, forEach, reject, equals } = require('ramda')
+
+const _Subject = function () {
+    this.observers = []
+}
+
+const Subject = () => new _Subject()
+
+const subscribe = (observer, subject) => {
+    subject.observers = append(observer, subject.observers)
+
+    const subscription = () => {
+        subject.observers = reject(equals(observer))(subject.observers)
+    }
+
+    return subscription
+}
+
+const next = (value, subject) => {
+    forEach(observer => observer.next(value))(subject.observers)
+}
+
+const error = (error, subject) => {
+    forEach(observer => observer.error(error))(subject.observers)
+}
+
+const complete = (subject) => {
+    forEach(observer => observer.complete())(subject.observers)
+}
+
+module.exports = {
+    Subject,
+    subscribe,
+    next,
+    error,
+    complete
+}
